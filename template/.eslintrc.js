af5efb9034238c2883b2ffb27d2fd790f5033705
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2017-04-30 15:37:55 +0300

    feat: Update deps

--- template/.eslintrc.js
+++ template/.eslintrc.js
@@ -23,6 +23,7 @@ module.exports = {
     // allow paren-less arrow functions
     'arrow-parens': 0,
     'one-var': 0,
+    'import/first': 0,
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
     'brace-style': [2, 'stroustrup', { 'allowSingleLine': true }]
