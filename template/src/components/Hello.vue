81c7d13cb618178836f01f8d37cca468b461c0ec
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2017-06-26 17:16:46 +0300

    Update Hello.vue

--- template/src/components/Hello.vue
+++ template/src/components/Hello.vue
@@ -19,6 +19,12 @@
     </q-toolbar>
 
     <div slot="left">
+      <!--
+        Use <q-side-link> component 
+        instead of <q-item> for 
+        internal vue-router navigation
+      -->
+
       <q-list no-border link inset-delimiter>
         <q-list-header>Essential Links</q-list-header>
         <q-item @click="launch('http://quasar-framework.org')">
@@ -37,12 +43,6 @@
           <q-item-side icon="rss feed" />
           <q-item-main label="Twitter" sublabel="@quasarframework" />
         </q-item>
-        
-        <!--
-          Use <q-side-link> component 
-          instead of <q-item> for 
-          internal vue-router navigation
-        -->
       </q-list>
     </div>
 
